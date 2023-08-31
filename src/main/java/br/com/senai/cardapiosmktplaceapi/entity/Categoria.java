c3abfd0748d87b7e4908753aeb30030459201cb2
Raul Nandi de Pieri <raul_pieri@estudante.sesisenai.org.br>
2023-08-30 22:23:48 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/senai/cardapiosmktplaceapi/entity/Categoria.java
@@ -0,0 +1,61 @@
+package br.com.senai.cardapiosmktplaceapi.entity;
+
+import br.com.senai.cardapiosmktplaceapi.entity.enums.Status;
+import br.com.senai.cardapiosmktplaceapi.entity.enums.TipoCategoria;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "categorias")
+@Entity(name = "Categoria")
+public class Categoria {
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+
+	@NotBlank(message = "O nome da categoria é obrigatório")
+	@Size(min = 5, max = 100, message = "O nome da categoria não deve vonter mais de 100 caracteres")
+	@Column(name = "nome")
+	private String nome;
+
+	@NotNull(message = "O status da categoria é obrigatório")
+	@Enumerated(value = EnumType.STRING)
+	@Column(name = "status")
+	private Status status;
+
+	@NotNull(message = "O tipo da categoria é obrigatório")
+	@Enumerated(value = EnumType.STRING)
+	@Column(name = "tipo")
+	private TipoCategoria tipo;
+	
+	public Categoria() {
+		this.status = Status.A;
+	}
+	
+	@Transient
+	public boolean isPersistido() {
+		return getId() != null && getId() > 0;
+	}
+	
+	@Transient
+	public boolean isAtivo() {
+		return getStatus() == Status.A;
+	}
+	
+}
---
+++ src/main/java/br/com/senai/cardapiosmktplaceapi/entity/enums/TipoCategoria.java
@@ -0,0 +1,8 @@
+package br.com.senai.cardapiosmktplaceapi.entity.enums;
+
+public enum TipoCategoria {
+	
+	RESTAURANTE,
+	OPCAO
+	
+}
